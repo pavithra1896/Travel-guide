a142c6f08f688153fd1ce00f90488fbd62730e05
pavithra <pavithra1896@gmail.com>
2019-09-04 07:21:29 +0000

    app,js file

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-//app.listen(process.env.PORT, process.env.IP, function(){
-app.listen(3000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
+//app.listen(3000, function(){
    console.log("The YelpCamp Server Has Started!");
 });
